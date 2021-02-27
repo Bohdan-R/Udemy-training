345a8a5e2ab91c2d85bba3910bb5420b531b0055
Bohdan Remeniuk <bogdanremenjuk@gmail.com>
2021-02-27 07:50:57 +0200

    add function in obj PMDB and work with array and solved task from the interview

---
+++ src/js/training.js
@@ -0,0 +1,31 @@
+// let x = 5; alert(x++);
+// равно 5, постфиксная запись сначала віводит число ка есть потом добавляет
+
+// [] + false - null + true
+/* console.log(typeof([] + false)); */
+// console.log([] + false - null + false); // NaN
+
+/* let y = 1;
+let x = y = 2;
+alert(x);  */
+
+// console.log([ ] + 1 + 2);
+
+// alert( "1264"[1] )
+
+// console.log(2 && 1 && null && 0 && undefined ); 
+// Оператор И запинается на лжи
+// Оператор ИЛИ запинается на правде
+
+/* console.log(!!( 1 && 2 ) === (1 && 2)); */
+
+/* alert( null || 2 && 3 || 4 ) */
+
+/* const a = [1, 2, 3];
+const b = [1, 2, 3];  */
+
+/* alert(typeof(+"Infinity")); */
+
+/* console.log("Ёжик" > "яблоко"); */
+
+/* console.log(0 || "" || 2 || undefined || true || falsе); */
