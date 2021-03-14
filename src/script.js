4c1b2c615d9b5b6f4ef126ba6b824aed7ba63195
Bohdan Remeniuk <bogdanremenjuk@gmail.com>
2021-03-14 09:19:13 +0200

    Rest operator and default parameters

--- src/script.js
+++ src/script.js
@@ -1,35 +1,7 @@
 'use strict';
 
-class Rectangle {
-    constructor(heigth, width) {
-        this.heigth = heigth;
-        this.width = width;
-    }
-
-    calcArea() {
-        return this.heigth * this.width
-    }
-};
-
-class ColoredRectangleWithText extends Rectangle {
-    constructor(heigth, width, text, bgColor) {
-        super(heigth, width);
-        this.text = text;
-        this.bgColor = bgColor;
-    }
-
-    showmyProps() {
-        console.log(`Текс: ${this.text}, цвет: ${this.bgColor}`);
-    }
+const log = function (a, b, ...args) {
+    console.log(a, b, args);
 }
 
-const div = new ColoredRectangleWithText(15, 25, 'Hello', 'red');
-
-div.showmyProps();
-console.log(div.calcArea());
-
-/* const square = new Rectangle(10, 10);
-const long = new Rectangle(20, 100);
-
-console.log(long.calcArea());
-console.log(square.calcArea()); */
+log(1, 2, 5, 6, 7,);
